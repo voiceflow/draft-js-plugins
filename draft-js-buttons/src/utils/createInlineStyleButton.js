29adec1fcc2b93c4355cb5c6f13075151ee0fac0
Evgeny <overhawlin@gmail.com>
2020-09-18 17:21:38 +0300

    apply inline style for the whole node if selection is collapsed

--- draft-js-buttons/src/utils/createInlineStyleButton.js
+++ draft-js-buttons/src/utils/createInlineStyleButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RichUtils } from 'draft-js';
+import { RichUtils, SelectionState, EditorState } from 'draft-js';
 
 export default ({ style }) => ({
   children,
@@ -9,7 +9,34 @@ export default ({ style }) => ({
   const onClick = React.useCallback(e => {
     e.preventDefault();
 
-    setEditorState(RichUtils.toggleInlineStyle(getEditorState(), style));
+    let editorState = getEditorState();
+    let selection = editorState.getSelection();
+
+    if (selection.isCollapsed()) {
+      const currentContent = editorState.getCurrentContent();
+      const blockMap = currentContent.getBlockMap();
+      const lastBlock = blockMap.last();
+      const firstBlock = blockMap.first();
+      const lastBlockKey = lastBlock.getKey();
+      const firstBlockKey = firstBlock.getKey();
+      const lengthOfLastBlock = lastBlock.getLength();
+      const prevSelection = selection;
+
+      selection = new SelectionState({
+        focusKey: lastBlockKey,
+        anchorKey: firstBlockKey,
+        focusOffset: lengthOfLastBlock,
+        anchorOffset: 0,
+      });
+
+      editorState = EditorState.acceptSelection(editorState, selection);
+      editorState = RichUtils.toggleInlineStyle(editorState, style);
+      editorState = EditorState.acceptSelection(editorState, prevSelection);
+
+      setEditorState(editorState);
+    } else {
+      setEditorState(RichUtils.toggleInlineStyle(editorState, style));
+    }
   }, []);
 
   const onMouseDown = React.useCallback(e => {
